a12424c4e4ef1d82668d5460df7f42e1bbe92a94
christiankoning <christiankoning001@fclive.nl>
2019-03-08 13:59:09 +0100

    Main menu, Pause game Added

---
+++ Prodigium/Prodigium/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Prodigium/Prodigium/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour {
+
+    public GameObject PausePanel;
+    public bool isPaused;
+
+    void start()
+    {
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if (isPaused)
+        {
+            pauseGame(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            pauseGame(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (Input.GetButtonDown("Cancel") || Input.GetKeyDown(KeyCode.JoystickButton7))
+        {
+            SwitchPause();
+        }
+    }
+
+    void pauseGame(bool state)
+    {
+        if (state)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+        PausePanel.SetActive(state);
+    }
+
+    public void SwitchPause()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+        }
+        else
+        {
+            isPaused = true;
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
